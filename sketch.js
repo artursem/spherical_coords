f43c47a968ecf16c476998c40c19b3e12d5782e8
Artur Semeniuk <artur.semeniuk@monterail.com>
2023-07-12 17:26:33 +0200

    Add slider - density

--- sketch.js
+++ sketch.js
@@ -1,25 +1,38 @@
 let r = 200;
+let density;
+let densitySlider;
 
 function setup() {
   createCanvas(700, 700, WEBGL);
   angleMode(DEGREES);
   colorMode(HSB);
 
-  stroke(0, 0, 100);
+  stroke(0, 0, 60);
   strokeWeight(3);
   noFill();
+
+  density = createDiv();
+  densitySlider = createSlider(3, 62, 24, 1)
 }
 
 function draw() {
   background(0, 0, 15);
   orbitControl(4, 4);
 
-  for(let phi = 0; phi < 180; phi += 5) {
-    for(let theta = 0; theta < 360; theta += 5) {
+  rotateY(90);
+  rotateZ(65);
+
+  for(let phi = 0; phi < 180; phi += 180/densitySlider.value()) {
+    beginShape()
+    for(let theta = 0; theta < 360; theta += 360/densitySlider.value()) {
       let x = r * cos(phi)
       let y = r * sin(phi) * sin(theta)
       let z = r * sin(phi) * cos(theta)
-      point(x, y, z)
+      vertex(x, y, z)
     }
+    endShape(CLOSE)
   }
+
+  let displayDensity = int(map(densitySlider.value(), 3, 62, 1, 60))
+  density.html("Density: " + displayDensity)
 }
